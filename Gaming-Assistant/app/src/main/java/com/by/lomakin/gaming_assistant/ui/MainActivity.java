559b04f7842742cf4c41a4fc78a9086fd5e4fcc8
Lomakin Vsevolod <lomakin_va_10@mf.grsu.by>
2016-11-17 16:21:40 +0300

    - Added Vk SDK - Added changes to support Vk SDK

--- Gaming-Assistant/app/src/main/java/com/by/lomakin/gaming_assistant/ui/MainActivity.java
+++ Gaming-Assistant/app/src/main/java/com/by/lomakin/gaming_assistant/ui/MainActivity.java
@@ -1,7 +1,11 @@
-package com.by.lomakin.gaming_assistant;
+package com.by.lomakin.gaming_assistant.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.by.lomakin.gaming_assistant.R;
+import com.vk.sdk.util.VKUtil;
 
 public class MainActivity extends AppCompatActivity {
 
